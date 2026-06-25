b7db77294108dfc6557572507c8c6673c9cfc8ec
ALI RAZA <alirazacui31@gmail.com>
2026-06-25 18:06:05 +0500

    fix settings

--- core/settings.py
+++ core/settings.py
@@ -83,14 +83,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # DATABASE_URL environment variable that dj_database_url picks up here.
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+    )
 }
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
