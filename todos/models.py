f700326226b637f3a5c0ea4238b6fb5e0efed7f0
ALI RAZA <alirazacui31@gmail.com>
2026-06-29 15:34:52 +0500

    new file added

--- todos/models.py
+++ todos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 
 class Task(models.Model):
+    
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
